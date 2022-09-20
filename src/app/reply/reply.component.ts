7460ca50c056a65bd3b65f25f004cdd7b0553c4d
Bairon Bernal <bairon.bernal@one-click.com.co>
2022-09-20 17:12:56 -0500

    desktop initial

--- src/app/reply/reply.component.ts
+++ src/app/reply/reply.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-reply',
   templateUrl: './reply.component.html',
-  styleUrls: ['./reply.component.scss']
+  styleUrls: ['../comment/comment.component.scss']
 })
 export class ReplyComponent implements OnInit {
 
