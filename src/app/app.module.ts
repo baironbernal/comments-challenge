03ccc5af24f112e56fe3a4e3d843a62d90f44adb
Bairon Bernal <bairon.bernal@one-click.com.co>
2022-09-20 15:47:43 -0500

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CommentComponent } from './comment/comment.component';
+import { CommentReplyComponent } from './comment-reply/comment-reply.component';
+import { ReplyComponent } from './reply/reply.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentComponent,
+    CommentReplyComponent,
+    ReplyComponent
   ],
   imports: [
     BrowserModule
